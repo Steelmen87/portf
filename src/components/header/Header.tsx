4559e99fa399607a63b4295acaacfb36d48b9403
Steel <Steelmen87@bk.ru>
2022-08-28 17:23:31 +0300

    Fixed the import sequence in Header

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import s from "./Header.module.css"
 import style from "../common/styles/Container.module.css"
+import s from "./Header.module.css"
 import Nav from "../nav/Nav";
 
 const Header = () => {
