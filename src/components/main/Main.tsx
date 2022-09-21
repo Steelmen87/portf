1ef1be54ce4d3e853526f93ff051bc245c7e437a
Steel <Steelmen87@bk.ru>
2022-09-21 10:40:41 +0300

    add photo

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import s from "./Main.module.css"
 import stylesContainer from "../common/styles/Container.module.css"
+import photo from '../common/img/main_photo.png'
 
 const Main = () => {
     return (
@@ -11,7 +12,9 @@ const Main = () => {
                     <h1>I am Petrov Konstantin</h1>
                     <p>Front end Developer</p>
                 </div>
-                <div className={s.photo}></div>
+                <div className={s.photo}>
+                    <img src={photo} alt='photo' width={'320px'}/>
+                </div>
             </div>
         </div>
     );
